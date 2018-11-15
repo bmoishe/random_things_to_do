c7ca41c713f896b7df45546bcaa4cb12e477c37a
bmoishe <moishebarnes@gmail.com>
2018-11-15 10:36:38 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# random_things_to_do
+
+API's used
+http://www.boredapi.com/api/activity/
+http://api.giphy.com
---
+++ random.js
@@ -0,0 +1,28 @@
+var ran = Math.floor((Math.random() * 4) + 1)
+var type
+var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var x = JSON.parse(this.responseText)
+      console.log(x.activity)
+      var type = x.type
+      console.log(x.type)
+      document.getElementById("idea").innerHTML = x.activity;
+    }
+  };
+  xhttp.open("GET", "http://www.boredapi.com/api/activity/", true);
+  xhttp.send();
+
+  var yhttp = new XMLHttpRequest();
+    yhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        var y = JSON.parse(this.responseText)
+        console.log(y.data[ran].embed_url)
+        console.log(y.data)
+        document.getElementById("gif").setAttribute('src', y.data[ran].images.downsized.url);
+      }
+    };
+
+  yhttp.open("GET", 'http://api.giphy.com/v1/gifs/search?q=' + type + '&api_key=ogH35pbi2E9fkl5ML2BMp6nvhiLzrHIF&limit=5', true);
+
+  yhttp.send();
